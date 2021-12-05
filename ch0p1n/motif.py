22202c67ace77de0068702cdb18f1a205cf82f78
flujoo <tnerrotana@gmail.com>
2021-12-05 17:18:14 +0400

    Add `replace`

--- ch0p1n/motif.py
+++ ch0p1n/motif.py
@@ -12,3 +12,20 @@ def flatten(motif):
                 points.append(cluster)
     
     return points
+
+
+def replace(motif, points):
+    """
+    Replace all the points in a motif on-site.
+    """
+    k = 0
+
+    for i, line in enumerate(motif):
+        for j, cluster in enumerate(line):
+            if isinstance(cluster, list):
+                l = len(cluster)
+                motif[i][j] = points[k:k+l]
+                k = k + l
+            else:
+                motif[i][j] = points[k]
+                k = k + 1
