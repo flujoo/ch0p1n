9f7914242b1a3a8e74a2ecbe0f085f0d63db631a
flujoo <tnerrotana@gmail.com>
2021-12-10 11:34:32 +0400

    Add `localize`

--- ch0p1n/transpose.py
+++ ch0p1n/transpose.py
@@ -1,4 +1,5 @@
-from ch0p1n.motif import flatten, replace
+from ch0p1n.motif import flatten, replace, modify, access
+from copy import deepcopy
 
 
 def transpose(pitch_motif, scale, step):
@@ -60,3 +61,19 @@ def transpose_pitch(pitch, scale, step):
     pitch = scale[i + step]
 
     return pitch
+
+
+def localize(pitch_motif, position, reference, scale, step):
+    """
+    Change the pitch at the given position,
+    according to some other pitch in the same pitch motif.
+    """
+    # get the referenced pitch
+    pitch = access(pitch_motif, reference)
+
+    # transpose the referenced pitch
+    scale = expand_scale(scale)
+    pitch = transpose_pitch(pitch, scale, step)
+
+    # modify the pitch at `position`
+    modify(pitch_motif, position, pitch)
