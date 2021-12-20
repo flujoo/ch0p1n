1dedf57be5c9dc43c81429662684a03d066a50bf
flujoo <tnerrotana@gmail.com>
2021-12-20 23:06:46 +0400

    Update test_motif.py

--- test/test_motif.py
+++ test/test_motif.py
@@ -5,14 +5,14 @@ from ch0p1n.motif import flatten, replace
 motif = [[60, 61], [62, [63, 64]]]
 
 
-class Test_flatten(unittest.TestCase):
+class TestFlatten(unittest.TestCase):
     def test(self):
         out = flatten(motif)
         expected = [60, 61, 62, 63, 64]
         self.assertEqual(out, expected)
 
 
-class Test_replace(unittest.TestCase):
+class TestReplace(unittest.TestCase):
     def test(self):
         out = replace(motif, [6, 6.1, 6.2, 6.3, 6.4])
         expected = [[6, 6.1], [6.2, [6.3, 6.4]]]
